c8b5dea5af6eeda193568edc25f7be7b8ec90b55
Rony cb <mylovepre16@gmail.com>
2020-06-03 19:34:07 -0500

    pages github

---
+++ build/precache-manifest.0e094196b726ad87e131e98cd7e8d716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1a2450907580a20faddfc2587acc8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d97613e5fd721ce4a45",
+    "url": "/static/css/main.836423be.chunk.css"
+  },
+  {
+    "revision": "b476896a693d3d99fcd2",
+    "url": "/static/js/2.c1417327.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1417327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d97613e5fd721ce4a45",
+    "url": "/static/js/main.24f8c6e2.chunk.js"
+  },
+  {
+    "revision": "14df0d56fde2e21a1810",
+    "url": "/static/js/runtime-main.b076f52a.js"
+  }
+]);
